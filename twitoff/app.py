6122412ee0fde41d4c2bc35be799c894f23f5661
bruno-janota <bruno.janota1@gmail.com>
2020-10-02 00:38:43 -0400

    Updated files after module 2 lecture

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,24 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from .db_model import DB, User
+from .twitter import add_user_tweepy
 
 
 def create_app():
     '''Create and configure an instance of our Flask application'''
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\bruno\\Desktop\\DSPT7-Twitoff\\twitoff.sqlite3'  # for absolute path
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\bruno\\Desktop\\DSPT7-Twitoff\\twitoff.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)  # Connect Flask app to SQLAlchemy DB
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff!'
+        return render_template('base.html', title='Home', users=User.query.all())
 
-    @app.route('/<username>/<followers>')
-    def add_user(username, followers):
-        user = User(username=username, followers=followers)
-        DB.session.add(user)
-        DB.session.commit()
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def add_or_update_user(name=None, message=''):
+        name = name or request.values['user_name']
 
-        return f'{username} has been added to the DB!'
+        try:
+            if request.method == "POST":
+                add_user_tweepy(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.username == name).one().tweet
+        except Exception as e:
+            print(f'Error adding {name}: {e}')
+            tweets = []
+            
+        return render_template('user.html', title=name, tweets=tweets, message=message)
 
     return app
